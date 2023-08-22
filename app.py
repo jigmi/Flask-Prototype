470c4bbb43630bb86123f428e77ccf98f484895e
root <jigmeydorjee1@protonmail.com>
2023-08-22 19:44:20 +1000

    Fixed and implemented features and error handling

--- app.py
+++ app.py
@@ -9,14 +9,16 @@ def index():
 @app.route("/signup")
 def signup():
     return render_template('signup.html')
+
 @app.route("/thankyou")
 def thankyou():
     first = request.args.get("first")
     last = request.args.get("last")
     return render_template("thankyou.html",first=first,last=last)
+
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html')
+    return render_template('404.html'),404
 
 if __name__ == '__main__':
     app.run(debug=True)
